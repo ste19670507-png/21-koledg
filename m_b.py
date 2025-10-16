d2814bb9128d6af8f74a57aa6a908d38e6896019
ste19670507-png <ste19670507@gmail.com>
2025-10-16 20:18:04 +0300

    Update m_b.py
    исправления 16,10 в 20-17

--- m_b.py
+++ m_b.py
@@ -1,3 +1,28 @@
 # морской бой
 # версия 1,1
+def zapolnit(spisok, simbol, razmer):
+    i = 0
+    j = 0
+    while j < razmer:
+        nev = []
+        i = 0
+        while i < razmer:
+            new.append(simbol)
+            i += 1
+        spisok.append(nev)
+        j += 1
+def pokazat(spisok, razmer):
+    i = 0
+    while i < razmer:
+        print(spisok [i])
+        print()
+        i = i + 1
+pole = []
+karta = []
+razmer = int(input("Введите размер таблицы:"))
+zapolnit(pole,0, razmer)
+zapolnit(karta,".", razmer)
+pokazat(pole, razmer)
+print()
+pokazat(karta, razmer)
 
